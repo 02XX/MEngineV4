7585e553c49bb6a854c1413ad7d32f651dfbc609
02 <1101533765@qq.com>
2025-09-09 23:37:18 +0800

    ✨ feat(Core): 添加数学模块
    1. 添加glm库

---
+++ Core/Math/include/Math.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/matrix_transform.hpp"
+#include "glm/glm.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "glm/gtx/matrix_decompose.hpp"
+#include "glm/matrix.hpp"
+#include "glm/trigonometric.hpp"
