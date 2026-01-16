2aa5635116afc8414bd7570a1d2bd57a50602c50
02 <1101533765@qq.com>
2026-01-16 09:12:24 +0800

    🐞 fix(Material): 修复初始化

--- Resource/Asset/src/RenderResource/PBRMaterialResource.cpp
+++ Resource/Asset/src/RenderResource/PBRMaterialResource.cpp
@@ -6,6 +6,11 @@ namespace MEngine::Resource
 {
 void PBRMaterialResource::InitRHI(std::shared_ptr<Context> context)
 {
+    auto pbrMaterial = static_cast<PBRMaterial *>(mOwnerAsset);
+    pbrMaterial->mTextures.Albedo->GetResource()->InitResource(context);
+    pbrMaterial->mTextures.Normal->GetResource()->InitResource(context);
+    pbrMaterial->mTextures.ARM->GetResource()->InitResource(context);
+    pbrMaterial->mTextures.Emissive->GetResource()->InitResource(context);
 }
 void PBRMaterialResource::ReleaseRHI(std::shared_ptr<Context> context)
 {
